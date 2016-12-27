3218a4e57630f8e3efe4f56d50e85efa68f78126
Samuel Gratzl <samuel_gratzl@gmx.at>
2016-12-27 12:36:59 +0100

    change imports according to core changes

--- src/parser.ts
+++ src/parser.ts
@@ -3,12 +3,12 @@
  */
 import {csv, text as d3text} from 'd3';
 import {IMatrix} from 'phovea_core/src/matrix';
-import {asMatrix, IAsMatrixOptions} from 'phovea_core/src/matrix_impl';
+import {asMatrix, IAsMatrixOptions} from 'phovea_core/src/matrix';
 import {ITable} from 'phovea_core/src/table';
-import {asTable as parseObjects, IAsTableOptions} from 'phovea_core/src/table_impl';
+import {asTable as parseObjects, IAsTableOptions} from 'phovea_core/src/table';
 
-export {asMatrix as parseMatrix} from 'phovea_core/src/matrix_impl';
-export {asTable as parseObjects, asTableFromArray as parseTable} from 'phovea_core/src/table_impl';
+export {asMatrix as parseMatrix} from 'phovea_core/src/matrix';
+export {asTable as parseObjects, asTableFromArray as parseTable} from 'phovea_core/src/table';
 
 export function parseRemoteMatrix(url: string, options: IAsMatrixOptions = {}): Promise<IMatrix> {
   return new Promise((resolve, reject) => {
