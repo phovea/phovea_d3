9704db14cd21ca32946cd545d7260b382e19765c
Martin Ennemoser <martin.ennemoser@jku.at>
2017-05-11 14:14:45 +0200

    Added typecheck

--- src/d3util.ts
+++ src/d3util.ts
@@ -25,7 +25,7 @@ export function transform(x = 0, y = 0, rotate = 0, scaleX = 1, scaleY = 1) {
  * @param selectOperation (optional) define selection operation directly (e.g., add, remove set)
  * @returns {function(any, any): undefined} the click handler
  */
-export function selectionUtil(data: IDataType, $data: d3.Selection<any>, selector: string, selectOperation?: SelectOperation) {
+export function selectionUtil(data: IDataType, $data: d3.Selection<any>, selector: string, selectOperation?: SelectOperation) : (d:any, i:number) => void {
 
   const l = function (event: any, type: string, selected: Range) {
     const all = $data.selectAll(selector);
