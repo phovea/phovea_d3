00f498d580ae2da5af52084121899af110bc4a45
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2020-01-30 07:38:36 +0100

    change reference types in tests to jest

--- tests/d3util.test.ts
+++ tests/d3util.test.ts
@@ -1,4 +1,4 @@
-/// <reference types="jasmine" />
+/// <reference types="jest" />
 import {transform} from '../src/d3util';
 
 describe('transform', () => {
--- tests/parser.test.ts
+++ tests/parser.test.ts
@@ -1,4 +1,4 @@
-/// <reference types="jasmine" />
+/// <reference types="jest" />
 import {parseMatrix} from '../src/parser';
 
 describe('parseMatrix', () => {
