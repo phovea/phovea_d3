0b208fe0cfdaf431192d40abf32daf4429e75397
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2020-02-05 14:06:47 +0100

    add additional test

--- tests/d3util.test.ts
+++ tests/d3util.test.ts
@@ -5,4 +5,8 @@ describe('transform', () => {
   it('empty', () => {
     expect(transform().toString()).toEqual('translate(0,0)rotate(0)skewX(0)scale(1,1)');
   });
+
+  it('with parameters', () => {
+    expect(transform(1, 2, 3, 4, 5).toString()).toEqual('translate(1,2)rotate(3)skewX(0)scale(4,5)');
+  });
 });
