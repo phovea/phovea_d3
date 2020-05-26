52671d85894fd95cbf22733e4a3f9734e1c607ab
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2020-05-26 08:21:44 +0200

    remove System.import()

--- phovea.js
+++ phovea.js
@@ -6,23 +6,23 @@
 
 //register all extensions in the registry following the given pattern
 module.exports = function(registry) {
-  //registry.push('extension-type', 'extension-id', function() { return System.import('./src/extension_impl'); }, {});
+  //registry.push('extension-type', 'extension-id', function() { return import('./src/extension_impl'); }, {});
 
-  registry.push('autoload', 'caleydo_links', function() { return System.import('./src/link_renderer'); }, {});
+  registry.push('autoload', 'caleydo_links', function() { return import('./src/link_renderer'); }, {});
 
-  registry.push('link-representation', 'link-block', function() { return System.import('./src/LinkRepresentation'); }, {
+  registry.push('link-representation', 'link-block', function() { return import('./src/LinkRepresentation'); }, {
   'name': 'Block Link Representation',
   'factory': 'createBlockRep',
   'granularity': 0
  });
 
-  registry.push('link-representation', 'link-group', function() { return System.import('./src/LinkRepresentation'); }, {
+  registry.push('link-representation', 'link-group', function() { return import('./src/LinkRepresentation'); }, {
   'name': 'Group Link Representation',
   'factory': 'createGroupRep',
   'granularity': 5
  });
 
-  registry.push('link-representation', 'link-item', function() { return System.import('./src/LinkRepresentation'); }, {
+  registry.push('link-representation', 'link-item', function() { return import('./src/LinkRepresentation'); }, {
   'name': 'Item Link Representation',
   'factory': 'createItemRep',
   'granularity': 10
