494569df42c3221f5d693d5c4f015f98abb9677c
anita-steiner <k9908600@gmx.at>
2020-06-02 07:30:54 +0200

    change registry entries

--- phovea_registry.js
+++ phovea_registry.js
@@ -4,7 +4,7 @@
  * Licensed under the new BSD license, available at http://caleydo.org/license
  **************************************************************************** */
 
-import {register} from 'phovea_core';
+import {PluginRegistry} from 'phovea_core';
 
 /**
  * build a registry by registering all phovea modules
@@ -12,4 +12,4 @@ import {register} from 'phovea_core';
 //other modules
 import 'phovea_core/phovea_registry.js';
 //self
-register('phovea_d3',require('./phovea.js'));
+PluginRegistry.getInstance().register('phovea_d3',require('./phovea.js'));
